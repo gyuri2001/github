f92716bb9900b707f8f55d40718205934337ced5
gyuri2001 <e15.buczko.gyorgy@palfy-vizugyi.hu>
2017-01-26 21:35:18 +0100

    feladat1

---
+++ PythonIdomar1.py
@@ -0,0 +1,47 @@
+'''
+F0001a: Mi történik, ha lefuttatjuk ezt a programot?
+A program futtatása nélkül fogalmazd meg magadnak – aztán persze lefuttathatod,
+hogy lásd, igazad volt-e:)
+'''
+print('...Hogyha én ezt tudtam volna)
+print('Köpönyeget hoztam vóna'!)
+
+#A program kiírja hogy, Köpönyeget hoztam vóna
+
+'''
+F0001b: Mi történik, ha lefuttatjuk ezt a programot?
+'''
+print('Rásír a sok kis malac is!')
+print('"Én is, én is, én is, én is!"')
+
+#A program elsősora ezt a szöveget fogja kiírni: Rásír a sok kis malac is!
+#A program második sora pedig ezt írjaki: "Én is, én is, én is, én is!"
+
+'''
+F0002a: Melyik állat nevét nem írja ki a program?
+'''
+cica = 'kutya'
+kutya = 'egér'
+egér = 'cica'
+print(cica)
+print('kutya')
+print(egér)
+print('cica')
+
+#az egeret nem írja ki
+
+'''
+F0002b: Mi lesz kiírva a program kimenetének utolsó sorában?
+'''
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+
+#egér
