e596758dd6c36a2f7ddbf72ea5ea42a2e81f38cc
gyuri2001 <e15.buczko.gyorgy@palfy-vizugyi.hu>
2017-02-02 18:02:09 +0100

    pythonidomar2

---
+++ pythonidomar2.py
@@ -0,0 +1,32 @@
+'''
+F0002b: Mi lesz kiírva a program kimenetének utolsó sorában?
+'''
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+
+#egér
+
+'''
+F0002c: Hány sornyi kiírás, azaz szöveg és üres sor kerül a képernyőre,
+ha majd egyszer lefuttatod ezt a programot?
+'''
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi?\n\n')
+print('Utolsó sor')
+
+#6 sor lesz összesen
+
+'''
+F0002d: Rajzold ki egyetlen print() utasítással az alábbi ábrát!
+'''
+print('     /\n   /\n  /\n  /\n/)
